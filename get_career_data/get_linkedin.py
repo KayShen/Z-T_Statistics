61f39f5bdfbe47edeb7ba4603b38dd7437373293
KayShen <kshen4@me.com>
2015-08-03 11:10:58 -0400

    add career data code

---
+++ get_career_data/get_linkedin.py
@@ -0,0 +1,7 @@
+import selenium,urllib,urllib2,random,time,os,re
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from bs4 import BeautifulSoup
+
