a8bc5a9d3cc994302ee867b1e1ec8b9ce59fa3f4
KayShen <kshen4@me.com>
2015-08-05 14:29:30 -0400

    add more

--- get_career_data/get_linkedin.py
+++ get_career_data/get_linkedin.py
@@ -74,8 +74,34 @@ def pull_down_page(driver,write_dir):
 	f.close()
 	return a_length
 
+def get_connection_name(driver):
+	soup=BeautifulSoup(driver.page_source, "lxml")
+	a=soup.find_all('h3',attrs={'class':'name'})
+	name_list = []
+	for i in range(len(a)):
+		print i
+		name_list.append(str(a[i].a.string.encode("UTF-8")))
+	return name_list
 
+def search_connection(driver,connection_name):
+	search_place = driver.find_elements_by_tag_name("input")[8]
+	ActionChains(driver).click(search_place).perform()
+	search_place2 = driver.find_elements_by_tag_name("input")[8]
+	ActionChains(driver).click(search_place2).send_keys(connection_name+"\n").perform()
+	return driver
 
+def find_same_name_connection_number(driver):
+	soup=BeautifulSoup(driver.page_source, "lxml")
+	a=soup.find_all('h3',attrs={'class':'name'})
+	return len(a)
+
+def click_connection(driver, connection_name):
+	driver.find_element_by_xpath("//a[contains(text(),\""+ connection_name + "\")]").click()
+	return(driver)
+
+def click_contact_info(driver):
+	driver.find_element_by_id("contacs-tab").click()
+	return(driver)
 
 driver = log_in(email_address, password)
 homepage = driver.current_url
@@ -90,9 +116,26 @@ print connection_page
 contact_count = find_contact_count(driver)
 write_dir = "/Users/Kay/Desktop/aaa.xml"
 found_contact = pull_down_page(driver,write_dir)
+name_list = get_connection_name(driver)
+driver.back()
 
 
+homepage = driver.current_url
+print homepage
+driver = go_connection(driver,homepage)
+driver.save_screenshot("/Users/Kay/Desktop/temp.png")
+connection_page = driver.current_url
+print connection_page
 
-
+for i in range(len(name_list)):
+	driver = search_connection(driver,name_list[i])
+	n_same_connection = find_same_name_connection_number(driver)
+	for j in range(n_same_connection):
+		driver = click_connection(driver, name_list[i])
+		soup=BeautifulSoup(driver.page_source, "lxml")
+		write_dir = "/Users/Kay/Desktop/aaa.xml"
+		f = open(write_dir, "w")
+		f.write(soup.prettify().encode("UTF-8"))
+		f.close()
 
 
